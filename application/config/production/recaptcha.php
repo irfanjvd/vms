cb930495fb1d9b5fc5722d40e289d831a2c70caf
Irfan Javed <irfan.javed@pitb.gov.pk>
2024-12-04 18:54:30 +0500

    debug

---
+++ application/config/production/recaptcha.php
@@ -0,0 +1,4 @@
+<?php
+$config['recaptcha_site_key'] = '6LeoUVgUAAAAACe3fbyV5zkM3MpfPg1SQa1IQ7vN';
+$config['recaptcha_secret_key'] = '6LeoUVgUAAAAAM0vNhD-B4EngeNUVzB2mdh3Z8Rb';
+?>
---
+++ application/config/recaptcha.php
@@ -0,0 +1,4 @@
+<?php
+$config['recaptcha_site_key'] = '6LfxU1gUAAAAAG2GYw3pS9dK29ElWrRV3cjSW8WS';
+$config['recaptcha_secret_key'] = '6LfxU1gUAAAAACxlu8pjXl5YZky__PqHsNJU5V5h';
+?>
